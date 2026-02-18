0fbf4fd896331713138b57b655ed0baba4102fdd
grilok777 <165169882+grilok777@users.noreply.github.com>
2026-02-18 09:11:00 +0200

    Practice_2
     Програмний калькулятор для розрахунку валових викидів шкідливих речовин у вигляді суспендованих твердих частинок при спалювання вугілля, мазуту та природного газу

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"math"
 	"net/http"
 	"strconv"
 )
@@ -14,7 +15,9 @@ type PageData struct {
 	// Для ПР1 Завдання 2 (Мазут)
 	InputMazut map[string]float64
 	ResMazut   map[string]float64
-
+	// Для ПР2
+	Input2    map[string]float64
+	Res2      map[string]float64
 
 	HasResult bool
 }
@@ -22,7 +25,7 @@ type PageData struct {
 func main() {
 	// Функція для парсингу шаблонів з підтримкою Layout
 	parse := func(file string) *template.Template {
-		return template.Must(template.ParseFiles("D:/Mysor2/4kurs/Prow_web_2/Pr1/template/layout.html", "template/"+file))
+		return template.Must(template.ParseFiles("templates/layout.html", "templates/"+file)) 
 	}
 
 	// Маршрут для ПР №1 (Калькулятор палива та мазуту)
@@ -89,11 +92,43 @@ func main() {
 				data.ResMazut["Hp"] = h * k_flam
 				data.ResMazut["Op"] = o * k_flam
 				data.ResMazut["Sp"] = s * k_flam
-				data.ResMazut["Ap"] = a * k_dry
+				data.ResMazut["Ap"] = a * (100 - w_val) / 100
 				data.ResMazut["Qri"] = q_dafi*(100-w_val-data.ResMazut["Ap"])/100 - 0.025*w_val
 			}
 		}
 		parse("pr1.html").ExecuteTemplate(w, "layout", data)
+	})
+
+	// Маршрут для ПР №2
+	http.HandleFunc("/pr2", func(w http.ResponseWriter, r *http.Request) {
+		data := PageData{Res2: make(map[string]float64)}
+		if r.Method == http.MethodPost {
+			b, _ := strconv.ParseFloat(r.FormValue("B"), 64)
+			n, _ := strconv.ParseFloat(r.FormValue("n"), 64)
+			av, _ := strconv.ParseFloat(r.FormValue("av"), 64)
+			fuel := r.FormValue("fuelType")
+
+			var qri, ar, hv float64
+			// Константи з вашого звіту ПР2
+			if fuel == "coal" {
+				qri, ar, hv = 20.47, 25.2, 1.5
+			} else if fuel == "oil" {
+				qri, ar, hv = 39.48, 0.15, 0.0
+			} else {
+				data.Res2["Kem"], data.Res2["E"] = 0, 0
+				data.HasResult = true
+				parse("pr2.html").ExecuteTemplate(w, "layout", data)
+				return
+			}
+
+			k_em := (math.Pow(10, 6) / qri) * av * (ar / (100 - hv)) * (1 - n)
+			e_val := 0.000001 * k_em * qri * b
+
+			data.Res2["Kem"] = k_em
+			data.Res2["E"] = e_val
+			data.HasResult = true
+		}
+		parse("pr2.html").ExecuteTemplate(w, "layout", data)
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
